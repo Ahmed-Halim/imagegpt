a6428479f83cae90871d47ec8ae20ea3f6f14f07
Halim <ahmed.halim162097@gmail.com>
2023-06-20 05:48:52 +0300

    init

---
+++ app.js
@@ -0,0 +1,61 @@
+const tesseract = require("node-tesseract-ocr");
+const multer = require("multer");
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const path = require("path");
+require("dotenv").config();
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  organization: process.env.OPENAI_ORG_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.static("dist"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./dist/index.html"));
+});
+
+const upload = multer({ dest: "./uploads/" });
+app.post("/ocr", upload.single("avatar"), (req, res) => {
+  tesseract
+    .recognize(req.file.path, {
+      lang: "eng",
+      oem: 1,
+      psm: 3,
+    })
+    .then((text) => {
+      res.send(text);
+    })
+    .catch((error) => {
+      res.send("err");
+    });
+});
+
+app.post("/gpt", async (req, res) => {
+  const question = req.body.question;
+  const prompt = req.body.prompt;
+  const reqBody = {
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content: prompt,
+      },
+      { role: "user", content: question },
+    ],
+  };
+  const completion = await openai.createChatCompletion(reqBody);
+  const answer = completion.data.choices[0].message.content;
+  res.send(answer);
+});
+
+app.listen(3000, () => {
+  console.log("app is running on port 3000");
+});
